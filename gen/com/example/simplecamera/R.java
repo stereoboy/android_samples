09a15dfbd18bb0675b4bf59075ba0438d0d33561
Jaeyoung Lee <jylee0121@gmail.com>
2014-12-13 02:49:35 +0900

    add a simple camera function

--- gen/com/example/simplecamera/R.java
+++ gen/com/example/simplecamera/R.java
@@ -13,6 +13,10 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int textView1=0x7f060000;
+        public static final int textureView1=0x7f060001;
+    }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
